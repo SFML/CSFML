d278df6912e6084effa26c26c4ea54183af9f3d7
Laurent Gomila <laurent.gom@gmail.com>
2012-02-11 18:51:25 +0100

    Updated Graphics.h

--- include/SFML/Graphics.h
+++ include/SFML/Graphics.h
@@ -30,17 +30,27 @@
 ////////////////////////////////////////////////////////////
 
 #include <SFML/Window.h>
+#include <SFML/Graphics/BlendMode.h>
+#include <SFML/Graphics/CircleShape.h>
 #include <SFML/Graphics/Color.h>
+#include <SFML/Graphics/ConvexShape.h>
 #include <SFML/Graphics/Font.h>
+#include <SFML/Graphics/Glyph.h>
 #include <SFML/Graphics/Image.h>
+#include <SFML/Graphics/PrimitiveType.h>
 #include <SFML/Graphics/Rect.h>
+#include <SFML/Graphics/RectangleShape.h>
+#include <SFML/Graphics/RenderStates.h>
 #include <SFML/Graphics/RenderWindow.h>
 #include <SFML/Graphics/RenderTexture.h>
 #include <SFML/Graphics/Shader.h>
 #include <SFML/Graphics/Shape.h>
-#include <SFML/Graphics/Sprite.h>
 #include <SFML/Graphics/Text.h>
 #include <SFML/Graphics/Texture.h>
+#include <SFML/Graphics/Transform.h>
+#include <SFML/Graphics/Transformable.h>
+#include <SFML/Graphics/Vertex.h>
+#include <SFML/Graphics/VertexArray.h>
 #include <SFML/Graphics/View.h>
 
 
