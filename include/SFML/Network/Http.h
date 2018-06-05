eb4a9cfe1def386132d45e7366926c731b74f15a
spacechase0 <spacechase0@users.noreply.github.com>
2018-06-05 13:55:21 -0400

    Update copyright year

--- include/SFML/Network/Http.h
+++ include/SFML/Network/Http.h
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 //
 // SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2007-2015 Laurent Gomila (laurent@sfml-dev.org)
+// Copyright (C) 2007-2018 Laurent Gomila (laurent@sfml-dev.org)
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
@@ -43,7 +43,7 @@ typedef enum
     sfHttpPost,  ///< Request in post mode, usually to send data to a page
     sfHttpHead,  ///< Request a page's header only
     sfHttpPut,   ///< Request in put mode, useful for a REST API
-    sfHttpDelete ///< Request in delete mode, useful for a REST API 
+    sfHttpDelete ///< Request in delete mode, useful for a REST API
 } sfHttpMethod;
 
 
