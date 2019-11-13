6d67570e556c39288799c43c284246e22908c06c
Jairard@Windows <gaetan.champarnaud@gmail.com>
2019-11-13 10:08:26 +0100

    Fix compilation and add check in sfTexture_updateFromTexture

--- src/SFML/Graphics/Texture.cpp
+++ src/SFML/Graphics/Texture.cpp
@@ -185,8 +185,9 @@ void sfTexture_updateFromPixels(sfTexture* texture, const sfUint8* pixels, unsig
 void sfTexture_updateFromTexture(sfTexture* destination, const sfTexture* texture, unsigned int x, unsigned int y);
 {
     CSFML_CHECK(texture);
+    CSFML_CHECK(texture->This);
 
-    CSFML_CALL_PTR(destination, update(texture->This, x, y));
+    CSFML_CALL_PTR(destination, update(*texture->This, x, y));
 }
 
 
