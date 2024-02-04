6f5bf37c17ef8baf373a38dc0ce84411166a4d1c
Chris Thrasher <chrisjthrasher@gmail.com>
2024-02-03 18:57:40 -0700

    Remove workarounds for old compilers

--- include/SFML/Config.h
+++ include/SFML/Config.h
@@ -93,20 +93,8 @@
 
 #else // Linux, FreeBSD, Mac OS X
 
-    #if __GNUC__ >= 4
-
-        // GCC 4 has special keywords for showing/hidding symbols,
-        // the same keyword is used for both importing and exporting
-        #define CSFML_API_EXPORT extern "C" __attribute__ ((__visibility__ ("default")))
-        #define CSFML_API_IMPORT CSFML_EXTERN_C __attribute__ ((__visibility__ ("default")))
-
-    #else
-
-        // GCC < 4 has no mechanism to explicitely hide symbols, everything's exported
-        #define CSFML_API_EXPORT extern "C"
-        #define CSFML_API_IMPORT CSFML_EXTERN_C
-
-    #endif
+    #define CSFML_API_EXPORT extern "C" __attribute__ ((__visibility__ ("default")))
+    #define CSFML_API_IMPORT CSFML_EXTERN_C __attribute__ ((__visibility__ ("default")))
 
 #endif
 
