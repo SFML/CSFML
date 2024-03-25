cc53c27566fd185365954bbc67986c8f06561dc5
Chris Thrasher <chrisjthrasher@gmail.com>
2024-03-25 14:50:45 -0600

    Add unit tests
    Yay tests!

---
+++ test/System/Sleep.test.cpp
@@ -0,0 +1,11 @@
+#include <SFML/System/Sleep.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("[System] sfSleep")
+{       
+    const auto startTime = std::chrono::steady_clock::now();
+    sfSleep(sfMilliseconds(10));
+    const auto elapsed = std::chrono::steady_clock::now() - startTime;
+    CHECK(elapsed >= std::chrono::milliseconds(10));
+}
---
+++ test/System/Vector3.test.cpp
@@ -0,0 +1,13 @@
+#include <SFML/System/Vector3.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("[System] sfVector3")
+{
+    SECTION("sfVector3f")
+    {
+        const sfVector3f vector{};
+        CHECK(vector.x == 0);
+        CHECK(vector.y == 0);
+    }
+}
