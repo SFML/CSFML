3415e32c6386f08207e0245656f6b5cebde9aa28
ZXShady <153229951+ZXShady@users.noreply.github.com>
2024-10-02 22:55:19 +0100

    Make ConvertEvent.hpp file in window module folder.
    It is a window related module it makes more sense imo for it to be in it.
    
    I also addded it to the cmake file so it appears in VIsual Studio

--- src/CSFML/Graphics/RenderWindow.cpp
+++ src/CSFML/Graphics/RenderWindow.cpp
@@ -25,7 +25,6 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include <CSFML/ConvertEvent.hpp>
 #include <CSFML/Graphics/CircleShapeStruct.hpp>
 #include <CSFML/Graphics/ConvertColor.hpp>
 #include <CSFML/Graphics/ConvertRect.hpp>
@@ -43,6 +42,7 @@
 #include <CSFML/Graphics/VertexBufferStruct.hpp>
 #include <CSFML/System/ConvertVector2.hpp>
 #include <CSFML/Window/ConvertContextSettings.hpp>
+#include <CSFML/Window/ConvertEvent.hpp>
 #include <CSFML/Window/ConvertVideoMode.hpp>
 #include <CSFML/Window/CursorStruct.hpp>
 
